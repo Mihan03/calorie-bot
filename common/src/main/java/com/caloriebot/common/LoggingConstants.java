7e86a074ce88cab6ddea181862559b5a8b0197be
Феоктистов Михаил Сергеевич <m.feoktistov@bssys.com>
2026-07-22 18:01:38 +0300

    Create common module and implemented simple logger

---
+++ common/src/main/java/com/caloriebot/common/LoggingConstants.java
@@ -0,0 +1,7 @@
+package com.caloriebot.common;
+
+public class LoggingConstants {
+    private LoggingConstants() {}
+
+    public static final String CORRELATION_ID = "correlationId";
+}
--- gateway/src/main/java/com/caloriebot/gateway/CalorieBot.java
+++ gateway/src/main/java/com/caloriebot/gateway/CalorieBot.java
@@ -1,7 +1,9 @@
 package com.caloriebot.gateway;
 
+import com.caloriebot.common.LoggingConstants;
 import lombok.Getter;
 import lombok.extern.slf4j.Slf4j;
+import org.slf4j.MDC;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.client.okhttp.OkHttpTelegramClient;
@@ -12,6 +14,8 @@ import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 import org.telegram.telegrambots.meta.generics.TelegramClient;
+
+import java.util.UUID;
 
 /**
  * @author Mikhail Feoktistov
@@ -37,21 +41,27 @@ public class CalorieBot implements SpringLongPollingBot, LongPollingSingleThread
 
     @Override
     public void consume(Update update) {
-        if (update.hasMessage() && update.getMessage().hasText()) {
-            String messageText = update.getMessage().getText();
-            long chatId = update.getMessage().getChatId();
+        MDC.put(LoggingConstants.CORRELATION_ID, UUID.randomUUID().toString());
+        try {
+            if (update.hasMessage() && update.getMessage().hasText()) {
+                String messageText = update.getMessage().getText();
+                long chatId = update.getMessage().getChatId();
+                log.info("Received update from chatId={}", chatId);
 
-            SendMessage message = SendMessage // Create a message object
-                    .builder()
-                    .chatId(chatId)
-                    .text(messageText)
-                    .build();
+                SendMessage message = SendMessage // Create a message object
+                        .builder()
+                        .chatId(chatId)
+                        .text(messageText)
+                        .build();
 
-            try {
-                telegramClient.execute(message); // Sending our message object to user
-            } catch (TelegramApiException e) {
-                log.error("Telegram API Exception", e);
+                try {
+                    telegramClient.execute(message); // Sending our message object to user
+                } catch (TelegramApiException e) {
+                    log.error("Telegram API Exception", e);
+                }
             }
+        } finally {
+            MDC.clear();
         }
     }
 
