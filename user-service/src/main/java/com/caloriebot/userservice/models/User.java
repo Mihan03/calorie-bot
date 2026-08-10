4650ca0cd8bf2cd63e2e33d745fbf8cf2ee5d7ee
Феоктистов Михаил Сергеевич <m.feoktistov@bssys.com>
2026-08-11 01:34:24 +0300

    add User entity and users table migration

---
+++ user-service/src/main/java/com/caloriebot/userservice/models/User.java
@@ -0,0 +1,22 @@
+package com.caloriebot.userservice.models;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.UUID;
+
+@Table(name = "users")
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+public class User extends AuditableEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private UUID id;
+
+    @Column(name = "tg_id", nullable = false)
+    private Long tgId;
+}
